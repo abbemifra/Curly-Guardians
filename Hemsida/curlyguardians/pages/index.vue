ab6898bf7787f2ea03da5319765ba954f17ecbb3
21ABBMax <max.wigstein@abbgymnasiet.se>
2022-09-07 10:04:37 +0200

    Ändringar

--- Hemsida/curlyguardians/pages/index.vue
+++ Hemsida/curlyguardians/pages/index.vue
@@ -1,5 +1,10 @@
 <template>
-  <div class="min-h-screen"></div>
+  <div class="min-h-screen">
+    <div
+      class="bg-fixed h-screen w-full bg-center bg-cover"
+      style="background-image: url(/images/WaterHomePage.jpg)"
+    ></div>
+  </div>
 </template>
 
 <script>
