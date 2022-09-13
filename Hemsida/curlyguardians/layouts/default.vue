136652d8c52460b22959a2da503f245ce4bad278
21ABBMax <max.wigstein@abbgymnasiet.se>
2022-09-13 12:21:39 +0200

    ändringar

---
+++ Hemsida/curlyguardians/layouts/default.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="">
+    <NavBar />
+    <nuxt></nuxt>
+    <FootComp />
+  </div>
+</template>
+
+<script>
+import NavBar from "../components/NavBar.vue";
+import FootComp from "~/components/FootComp.vue";
+export default {
+  data() {
+    return {
+      NavBar,
+    };
+  },
+  components: { NavBar, FootComp },
+};
+</script>
